96c2275dcd287f665bc25169fdfffea00008b10c
Daniela Regert <danielaregert@MacBook-Pro-de-Daniela.local>
2025-05-22 21:18:24 -0300

    tailwind added

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+
+import { h } from 'preact';
 import { useTranslation } from 'react-i18next';
 
 export default function App() {
@@ -12,12 +10,24 @@ export default function App() {
   };
 
   return (
-    <div>
-      <h1>{t('welcome')}</h1>
-      <p>{t('description')}</p>
-      <button onClick={toggleLang}>
-        {i18n.language === 'es' ? 'Switch to English' : 'Cambiar a Español'}
-      </button>
-    </div>
+    
+
+    <main class="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6 gap-6">
+      <div>
+        <h1 class="text-4xl font-bold text-blue-600 mb-4 font-sans">{t('welcome')}</h1>
+      </div>
+
+      <div class="text-center">
+        <p class="mt-4 text-gray-700">{t('description')}</p>
+        <h1 class="text-3xl font-semibold mb-2"></h1>
+        <p class="text-lg mb-4">{t('description')}</p>
+        <button
+          class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+          onClick={toggleLang}
+        >
+          {i18n.language === 'es' ? 'Switch to English' : 'Cambiar a Español'}
+        </button>
+      </div>
+    </main>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { render } from 'preact';
 import { I18nextProvider } from 'react-i18next';
 import App from './app';
 import './i18n'; // Esto asegura que i18n se inicialice
+import './index.css';
 
 render(
   <I18nextProvider i18n={import('./i18n').default}>
